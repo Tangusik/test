d83478168fa1303fa33a809fc823be4fb3199572
LosPiesDescalzos <katyaj2001@mail.ru>
2023-06-27 12:36:41 +0300

    add styles for buttons

---
+++ static/js/buttons.js
@@ -0,0 +1,18 @@
+function showGroupForm() {
+    var form = document.getElementById("form_add_team");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+
+function showClientForm() {
+    var form = document.getElementById("form_add_client");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
