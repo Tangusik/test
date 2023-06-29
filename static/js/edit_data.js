97b759989096d6fe0ac156fedcc193352edb0407
LosPiesDescalzos <katyaj2001@mail.ru>
2023-06-29 18:39:15 +0300

    add edit buttons to main and clients_info

--- static/js/edit_data.js
+++ static/js/edit_data.js
@@ -1,4 +1,11 @@
-function edit_data(){
-      var modal = document.getElementById("");
+let modal = document.querySelector('.modal');
 
+function edit_data() {
+    modal.style.display = 'block';
+}
+
+let btn = document.querySelector('.close');
+
+btn.onclick = function () {
+    modal.style.display = 'none';
 }
